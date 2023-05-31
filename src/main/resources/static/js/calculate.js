f040e30b861a6501def3e5682b3fb5fcaf6cfcda
Edmund Lee <75547137+leeedmund@users.noreply.github.com>
2023-05-31 07:42:36 -0700

    error checking

--- src/main/resources/static/js/calculate.js
+++ src/main/resources/static/js/calculate.js
@@ -1,3 +1,6 @@
+
+// Variables
+
 var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03,
     49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 
@@ -13,7 +16,11 @@ var c = document.getElementById("C");
 var cminus = document.getElementById("C-");
 var d = document.getElementById("D");
 var f = document.getElementById("F");
-            
+var lowestLim = 0;
+var currentLow = f.value;
+var message;
+
+
 var intervals = [];
 intervals.push(f.value,d.value,cminus.value,c.value,cplus.value,bminus.value,
     b.value,bplus.value,aminus.value,a.value,aplus.value,max.value);
@@ -22,14 +29,66 @@ var strings = [];
 strings.push(aplus.id,a.id,aminus.id,bplus.id,
     b.id,bminus.id,cplus.id,c.id,cminus.id,d.id,f.id);
 
+// Functions
+
 function compareTo (x, y) {
     return x > y;
 }
 
 function printMsg() {
-    var message = "Bounds must be valid. Only numbers allowed";
+    message = "Bounds must be valid. Only numbers allowed";
     document.getElementById("error-msg").innerHTML = message;    
 }
+
+function generateHistogram() {
+    if (!isLowest()) {
+        message = "Lowest Grade Percentage is not set to 0";
+        document.getElementById("error-msg").innerHTML = message;    
+    } else {
+        var count = [];
+        for (let i = (strings.length-1); i >= 0; i--) {
+            var counter = 0;
+            var lowerBound = intervals[i];
+            for (let j = 0; j < grades.length; j++) {
+                if (i == (strings.length-1)) {
+                    if (grades[j] >= Number(lowerBound)) {
+                        counter++;
+                    }
+                } else {
+                    var upperBound = intervals[i+1]
+                    if ((grades[j] >= Number(lowerBound)) && (grades[j] < upperBound)) {
+                        counter++;
+                    }
+                }
+            }
+            count.push(counter);
+        }
+        
+        var listOfHist = document.getElementsByClassName("hist");
+        for (let i = 0; i < count.length; i++) {
+            var tempVal = count[i];
+            var space = "";
+            for (let j = 0; j < tempVal; j++) {
+                space = space + "X";
+            }
+            listOfHist[i].innerHTML = space;
+        }
+    }
+}
+
+function isOutOfBounds(x) {
+    return ((x > Number(max.value)) || (x < lowestLim));
+}
+
+function isLowest() {
+    return currentLow == lowestLim;
+}
+
+function valueAdded() {
+    message = "Number has been successfully added.";
+    document.getElementById("error-msg").innerHTML = message;   
+}
+
 
 for (let i = 0; i < intervals.length-1; i++) {
     var a = Number(intervals[i]);
@@ -41,36 +100,26 @@ for (let i = 0; i < intervals.length-1; i++) {
 
 intervals.pop();
 
-function generateHistogram() {
-    var count = [];
-    for (let i = (strings.length-1); i >= 0; i--) {
-        var counter = 0;
-        var lowerBound = intervals[i];
-        for (let j = 0; j < grades.length; j++) {
-            if (i == (strings.length-1)) {
-                if (grades[j] >= Number(lowerBound)) {
-                    counter++;
-                }
-            } else {
-                var upperBound = intervals[i+1]
-                if ((grades[j] >= Number(lowerBound)) && (grades[j] < upperBound)) {
-                    counter++;
-                }
-            }
-        }
-        count.push(counter);
-    }
-    
-
-    var listOfHist = document.getElementsByClassName("hist");
-    for (let i = 0; i < count.length; i++) {
-        var tempVal = count[i];
-        var space = "";
-        for (let j = 0; j < tempVal; j++) {
-            space = space + "X";
-        }
-        listOfHist[i].innerHTML = space;
-    }
-}
 
 generateHistogram();
+
+var input = document.getElementById("newgrade");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        var addValue = input.value;
+        event.preventDefault();
+        if(input.value.length == 0) { 
+            message = "Please enter a valid number.";
+            document.getElementById("error-msg").innerHTML = message;    
+        } else {
+            if (isOutOfBounds(addValue)) {
+                message = "Number is out of bounds. Please try again";
+                document.getElementById("error-msg").innerHTML = message;   
+            } else {
+                grades.push(Number(addValue));
+                generateHistogram();
+                valueAdded();
+            }
+        }
+    }
+});
